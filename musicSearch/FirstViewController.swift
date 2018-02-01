af9e099b6900b0d97cc298333c7a99e2986dfae7
BobDeKort <dekort.bob101@gmail.com>
2018-02-01 13:42:54 -0800

    initial commit

---
+++ musicSearch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  musicSearch
+//
+//  Created by Bob De Kort on 2/1/18.
+//  Copyright © 2018 Bob De Kort. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ musicSearch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  musicSearch
+//
+//  Created by Bob De Kort on 2/1/18.
+//  Copyright © 2018 Bob De Kort. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
